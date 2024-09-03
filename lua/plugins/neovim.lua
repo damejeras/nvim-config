fb7f2b46a2b0d4a5b4419335f9451756efa149bc
Darius Mejeras <darius@mejeras.lt>
2024-09-03 09:43:04 +0300

    Add CodeCompanion

--- lua/plugins/neovim.lua
+++ lua/plugins/neovim.lua
@@ -171,4 +171,45 @@ return {
     version = false,
     config = {}
   },
+
+  -- Enable ChatGPT
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim",      -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "openai",
+        },
+        inline = {
+          adapter = "openai",
+        },
+        agent = {
+          adapter = "openai",
+        },
+      },
+      adapters = {
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            env = {
+              api_key = "cmd:op read op://personal/openai/credential --no-newline",
+            },
+            schema = {
+              model = {
+                default = "gpt-4o-mini"
+              }
+            }
+          })
+        end,
+      },
+    }
+  }
 }
