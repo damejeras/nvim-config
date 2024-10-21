483c66e1bb254c47429a1ca3941bf6d48bbad720
Darius Mejeras <darius@mejeras.lt>
2024-10-21 09:34:36 +0300

    another llm

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,47 +1,43 @@
 return {
   {
-    "yetone/avante.nvim",
-    event = {
-      "VeryLazy"
-    },
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    "olimorris/codecompanion.nvim",
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp",                      -- Optional: For using slash commands and variables in the chat buffer
+      "nvim-telescope/telescope.nvim",         -- Optional: For using slash commands
+      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "anthropic",
         },
+        inline = {
+          adapter = "openai",
+        },
+        agent = {
+          adapter = "openai",
+        },
+
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:op read op://Personal/anthropic/credential --no-newline"
+            },
+          })
+        end,
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            env = {
+              api_key = "cmd:op read op://Personal/openai/credential --no-newline",
+            },
+          })
+        end,
       },
     },
+    config = true
   }
 }
