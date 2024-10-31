923fb593f7dc0746dedddf306391d9c803313fad
Darius Mejeras <darius@mejeras.lt>
2024-10-31 10:46:26 +0200

    Configure spelling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,65 +1,69 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader       = ' '
+vim.g.maplocalleader  = ' '
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 --
 -- A TAB character looks like 4 spaces
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
+vim.o.tabstop         = 4
+vim.o.shiftwidth      = 4
+vim.o.softtabstop     = 4
 
 -- Set highlight on search
-vim.o.hlsearch = true
+vim.o.hlsearch        = true
 
 -- Make line numbers default
-vim.wo.number = true
+vim.wo.number         = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse           = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard       = 'unnamedplus'
 
 -- Enable break indent
-vim.o.breakindent = true
+vim.o.breakindent     = true
 
 -- Save undo history
-vim.o.undofile = true
+vim.o.undofile        = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.ignorecase      = true
+vim.o.smartcase       = true
 
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+-- Keep sign column on by default
+vim.wo.signcolumn     = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.updatetime      = 250
+vim.o.timeoutlen      = 300
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+-- Set complete opt to have a better completion experience
+vim.o.completeopt     = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.o.termguicolors   = true
 
--- Highligh current line
-vim.o.cursorline = true
+-- Highlight current line
+vim.o.cursorline      = true
 
 -- Show 100 symbols line
-vim.o.colorcolumn = '100'
+vim.o.colorcolumn     = '100'
 
 -- Enable line wrap
-vim.o.wrap = true
+vim.o.wrap            = true
 
 -- Open splits on the right
-vim.o.splitright  = true
+vim.o.splitright      = true
 
+-- Configure spellchecking
+vim.o.spelllang       = 'en_us'
+vim.o.spellcapcheck   = ''
+vim.o.spell           = true
