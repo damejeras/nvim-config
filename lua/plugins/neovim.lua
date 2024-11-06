af2384cb392cfdd8ddb0a6bb0882adf217a76655
Darius Mejeras <darius@mejeras.lt>
2024-11-06 13:38:21 +0200

    Add icons

--- lua/plugins/neovim.lua
+++ lua/plugins/neovim.lua
@@ -187,17 +187,13 @@ return {
   {
     'romgrk/barbar.nvim',
     dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
     },
     init = function()
       vim.g.barbar_auto_setup = false
     end,
     opts = {
-      icons = {
-        filetype = {
-          enabled = false
-        }
-      }
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
