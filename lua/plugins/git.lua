244b4c759cc1c7fbb2521101ec544e920b583d86
Darius Mejeras <darius@mejeras.lt>
2024-12-17 16:26:44 +0200

    Add icons

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,12 @@
 return {
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'shumphrey/fugitive-gitlab.vim',
-
+  {
+    'tpope/vim-fugitive',
+    dependencies = {
+      'shumphrey/fugitive-gitlab.vim',
+      'tpope/vim-rhubarb',
+    },
+  },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
 
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
