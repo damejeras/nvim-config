7b93f336d0e2abcfb272272a1e6a04d199f57aa2
Darius Mejeras <damejeras@users.noreply.github.com>
2025-01-17 16:19:15 +0200

    Add copilot

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -80,5 +80,24 @@ return {
       },
     },
     config = true
+  },
+  {
+    "github/copilot.vim",
+    config = function()
+      -- disable tab mapping
+      vim.g.copilot_no_tab_map = true
+
+      -- next suggestion
+      vim.keymap.set('i', '<C-J>', 'copilot#Next()', {
+        expr = true,
+        replace_keycodes = false
+      })
+
+      -- accept suggested
+      vim.keymap.set('i', '<C-F>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+    end
   }
 }
