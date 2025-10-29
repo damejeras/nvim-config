55328ab72ffe641adb0dd9b2222b4fa4d2d910f0
Darius Mejeras <darius@mejeras.lt>
2025-10-29 18:26:25 +0200

    Configure formatters

---
+++ lua/plugins/formatters.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"goimports",
+				"gofumpt",
+				"stylua",
+				"prettier",
+			},
+			auto_update = false,
+			run_on_start = true,
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		opts = {
+			-- Define formatters for each filetype
+			formatters_by_ft = {
+				go = { "goimports", "gofumpt" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				terraform = { "terraform_fmt" },
+			},
+			-- Format on save
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+}
